4882449d39b8bb98084696b20ff74452b5969e46
karl <karlbayron18@gmail.com>
2022-02-26 00:04:08 +0800

    Added Expense form, and passed the value of the form up to the parent until it reaches App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import Expenses from "./components/Expenses/Expenses";
-
+import NewExpense from "./components/NewExpense/NewExpense";
 const App = () => {
   const expense = [
     { title: "Car Insurance", amount: 244.67, date: new Date(2022, 4, 28) },
     { title: "Home Insurance", amount: 214.67, date: new Date(2019, 7, 28) },
     { title: "Fire Insurance", amount: 194.67, date: new Date(2005, 10, 28) },
   ];
+
+  const addExpenseHandler = (enteredExpenseData) => {
+    console.log(enteredExpenseData);
+  }
   return (
     <div>
-      <h2> Let's get started! </h2>
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses expense={expense} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,10 +2,15 @@ import "./ExpenseItem.css";
 import "./ExpenseDate";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
+import { useState } from "react";
 const ExpenseItem = (props) => {
-  const expenseTitle = props.title;
+  const [expenseTitle,setTitle] = useState(props.title);
+
   const expenseAmount = props.amount;
 
+  const clickHandler = () => {
+    setTitle("Updated!");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate expenseDate={props.date} />
@@ -13,7 +18,8 @@ const ExpenseItem = (props) => {
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 export default ExpenseItem;
